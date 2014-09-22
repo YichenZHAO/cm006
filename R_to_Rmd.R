3af2cfc78ef9ec39805087bbcc4b6cfeded6ac67
YichenZHAO <zhaoyc86@hotmail.com>
2014-09-22 10:52:47 -0700

    no message

--- R_to_Rmd.R
+++ R_to_Rmd.R
@@ -1,5 +1,5 @@
 #'---
-#'  title: "cm006"
+#'title: "cm006"
 #'author: "Yichen Zhao"
 #'date: "Monday, September 22, 2014"
 #'output: html_document
@@ -9,14 +9,15 @@
 
 #'  When you click the **Knit** button a document will be generated that includes both content as well as the output of any embedded R code chunks within the document. You can embed an R code chunk like this:
   
-
-summary(cars)
+gdURL <- "http://tiny.cc/gapminder"
+gDat <- read.delim(file = gdURL) 
+summary(gDat)
 
 
 #'You can also embed plots, for example:
   
 
-plot(cars)
+plot(gDat)
 
 
 #'Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
--- cm006.R
+++ cm006.R
@@ -1,3 +1,5 @@
+# Refer to: https://github.com/jennybc/ggplot2-tutorial/blob/master/gapminder-ggplot2-scatterplot.md
+
 library("ggplot2")
 
 gdURL <- "http://tiny.cc/gapminder"
@@ -10,3 +12,38 @@ ggplot(gDat, aes(x = gdpPercap, y = lifeExp))
 # plot by + geom
 p <- ggplot(gDat, aes(x = gdpPercap, y = lifeExp)) # just initializes
 p + geom_point()
+
+## Log transform
+# bad way: direct log
+ggplot(gDat, aes(x = log10(gdpPercap), y = lifeExp)) + geom_point()
+# better way to do this!
+p + geom_point() + scale_x_log10() # p is defined previously, here we just need to scale x!
+
+# Name axies
+p + geom_point() + scale_x_log10() + xlab("GDP Per Capita") + ylab("Life Expectancy")
+
+##  Define new ## 
+pp <- p + scale_x_log10() + xlab("log (GDP Per Capita)") + ylab("Life Expectancy")
+pp + geom_point()
+
+# new aesthetic (e.g. with color)
+pp + geom_point(aes(color = continent)) 
+
+# SET alpha transparency and size to a value
+pp + geom_point(alpha = (1/3), size = 3) 
+pp + geom_point(alpha = (1/3), size = 1) 
+pp + geom_point(alpha = (1/10), size = 3) 
+
+# add a fitted curve or line
+pp + geom_point() + geom_smooth()  #the grey part is something like an inference
+pp + geom_point() + geom_smooth(lwd = 3, se = FALSE)
+pp + geom_point() + geom_smooth(lwd = 3, se = FALSE, method = "lm")
+
+# revive our interest in continents (levels of one variable)
+pp + aes(color = continent) + geom_point() + geom_smooth(lwd = 3, se = FALSE)
+
+# facetting: another way to exploit a factor
+pp + geom_point(alpha = (1/3), size = 3) + facet_wrap(~ continent)
+pp + geom_point(alpha = (1/3), size = 3) + facet_wrap(~ continent) +
+  geom_smooth(lwd = 2, se = FALSE)
+
